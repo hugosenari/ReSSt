b73be45ca8add97f1c25ecb5effdfa94612e55f5
Hugo Ribeiro <hugo.ribeiro@net.com.br>
2017-10-03 16:33:42 -0300

    crud for plugins

--- ReSStUI/settings/plugins/component.js
+++ ReSStUI/settings/plugins/component.js
@@ -9,38 +9,33 @@ window.ReSSt.settings.plugins = Promise.resolve({
         };
     },
     created () {
+        const app = window.ReSSt.App;
+        app.$on('LoadPlugin', this.updatePluginList);
+        app.$on('RemovePlugin', this.updatePluginList);
+        app.$on('PluginReady', this.updatePluginList);
+        app.$on('PluginEnabled', this.updatePluginList);
+        app.$on('PluginDisabled', this.updatePluginList);
         this.updatePluginList();
     },
     watch: {},
     methods: {
-        loadPlugin(plugin_name, plugin_address) {
-            const app = window.ReSSt.App;
-            app.$emit('LoadPlugin', plugin_name, plugin_address);
-            app.$on('LoadPlugin', this.updatePluginList);
-            app.$on('RemovePlugin', this.updatePluginList);
-            app.$on('PluginReady', this.updatePluginList);
-            app.$on('PluginEnabled', this.updatePluginList);
-            app.$on('PluginDisabled', this.updatePluginList);
-            this.updatePluginList();
-        },
-        unloadPlugin(name) {
-            const app = window.ReSSt.App;
-            app.$emit('RemovePlugin', name);
-            this.updatePluginList();
-        },
-        changePluginState(name, state) {
-            const app = window.ReSSt.App;
-            if (state) {
-                app.$emit('PluginEnabled', name);
-            } else {
-                app.$emit('PluginDisabled', name);
-            }
-            this.updatePluginList();
-        },
         updatePluginList() {
             const app = window.ReSSt.App;
             const methods = app.$options.methods;
             this.plugins = methods.getPlugins();
+        },
+        loadPlugin(plugin_name, plugin_address) {
+            const app = window.ReSSt.App;
+            app.$emit('LoadPlugin', plugin_name, plugin_address);
+        },
+        unloadPlugin(name) {
+            const app = window.ReSSt.App;
+            app.$emit('RemovePlugin', name);
+        },
+        changePluginState(name, state) {
+            const app = window.ReSSt.App;
+            const eventName =  state ? 'PluginEnabled' : 'PluginDisabled';
+            app.$emit(eventName, name);
         }
     }   
 });
