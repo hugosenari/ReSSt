9665b5330f686f7be20a407a03ac17011a916ae3
hugosenari <hugosenari@gmail.com>
2017-10-04 03:00:00 -0200

    add gfycat plugin

--- ReSStUI/plugins/gfycat.js
+++ ReSStUI/plugins/gfycat.js
@@ -3,16 +3,15 @@
 (() => {
     let enabled = false;
     const app = window.ReSSt.App;
-    const name = 'imgurian';
-    const getImagurBtn = path => {
-        var id = path.replace(/.+imgur\.com\/([^.#?]+)\.*.*/, '$1');
-        id = id.replace(/gallery\//, '');
-        return !id ? '':`<blockquote class="imgur-embed-pub" lang="en" data-id="${id}" data-context="false">
+    const name = 'gfycat';
+    const getBtn = path => {
+        var id = path.replace(/.+gfycat\.com\/([^.#?]+)\.*.*/, '$1');
+        return !id ? '':`<div class="gfyitem" data-title=true data-autoplay=true data-controls=true data-id="${id} data-responsive=true">
     <md-button class="md-fab md-mini md-raised" title="show this"
-        onclick="(() => {const script = document.createElement('script'); script.src = '//s.imgur.com/min/embed.js'; body.appendChild(script);})();">
+        onclick="(() => {window.gfyCollection.init();})();">
         <i class="material-icons" style="font-size:44px;">play_circle_outline</i>
     </md-button>
-</blockquote>`;
+</div>`;
     };
     const getUrls = content => {
         const urls = [];
@@ -21,7 +20,7 @@
             if (matches) {
                 for (const match of matches){
                     const url = match.replace(/href=["']/, '');
-                    if (url.match(/.+imgur\.com\//)){
+                    if (url.match(/.+gfycat\.com\//)){
                         urls.push(url);
                     }
                 }
@@ -29,7 +28,18 @@
         }
         return urls;
     };
-    app.$on('PluginEnabled', n => n === name && (enabled = true));
+    app.$on('PluginEnabled', n => {
+        if (n === name) {
+            enabled = true;
+            (function(d, s, id){
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {return;}
+                js = d.createElement(s); js.id = id;
+                js.src = "https://assets.gfycat.com/gfycat.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'gfycat-js'));
+        }
+    });
     app.$on('PluginDisabled', n => n === name && (enabled = false));
     app.$on('BeforeShowItem', item => {
         if (enabled) {
@@ -40,8 +50,8 @@
                     urls = urls.concat(getUrls(item.content[i]));
                 }
                 for (const url of urls) {
-                    item.content.push(getImagurBtn(url));
-                } 
+                    item.content.push(getBtn(url));
+                }
             }
         }
         return item;
