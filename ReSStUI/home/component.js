3eab6c4e9b5900adf3ca17b5538bd94e335e7b0c
hugosenari <hugosenari@gmail.com>
2018-01-04 11:25:05 -0200

    add share url

--- ReSStUI/home/component.js
+++ ReSStUI/home/component.js
@@ -3,8 +3,18 @@ window.ReSSt.home = window.ReSSt
     .mapState('key', 'endpoint')
     .then(computed => (
         {
+            created () { this.apiFromQuery(); },
+            watch: { '$route': 'apiFromQuery' },
             computed,
             methods: {
+                apiFromQuery () {
+                    const query = this.$route.query;
+                    if(query.key && query.endpoint) {
+                        computed.endpoint.set(query.endpoint);
+                        computed.key.set(query.key);
+                        this.$router.push('feeds');
+                    }
+                },
                 onSave () { this.$router.push('feeds'); }
             }
         }
--- ReSStUI/settings/service/component.js
+++ ReSStUI/settings/service/component.js
@@ -1,4 +1,14 @@
 /*jshint esversion: 6 */
 window.ReSSt.settings.service = window.ReSSt
     .mapState('key', 'endpoint', 'showReaded')
-    .then(computed => ({ computed } ));
+    .then(computed => ({
+        computed: Object.assign(
+            {
+                sharelogin: function () {
+                    const index = `${window.location.origin}${window.location.pathname}`;
+                    return `${index}#?key=${this.key}&endpoint=${this.endpoint}`;
+                }
+            },
+            computed
+        )
+    }));
