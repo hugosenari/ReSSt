37ccd334e528fbca0044d4bbc60c94eefc38beb9
Hugo Ribeiro <hugo.ribeiro@net.com.br>
2018-01-04 01:12:50 -0200

    add perfomance info

--- ReSStUI/settings/service/component.js
+++ ReSStUI/settings/service/component.js
@@ -1,13 +1,4 @@
 /*jshint esversion: 6 */
 window.ReSSt.settings.service = window.ReSSt
     .mapState('key', 'endpoint', 'showReaded')
-    .then(computed => (
-        { 
-            data () {
-                return {
-                    time_to_run: new Date().getTime() - window.APP_START_TIME
-                };
-            },
-            computed
-        }
-    ));
+    .then(computed => ({ computed } ));
